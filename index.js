279c39e14a0838d3f9a21cde7088bac6402e5808
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-09-19 12:17:20 +0000

    Added logger middleware

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+function getTime() {
+	const date = new Date();
+
+	let dateAndTime = date.toLocaleString('en-US', {
+		timeZone: 'America/New_York',
+	});
+
+	return dateAndTime;
+}
+
+app.use(function logger(req, res, next) {
+	console.log(`${getTime()} EST - ${req.method} ${req.path} - ${req.ip}`);
+	console.log("")
+	next();
+});
+
 app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}/views/index.html`);
 });
