33e75478b9729d7a0e77ff554fb97aee48619243
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-12 18:35:25 -0400

    Added module export to index.js

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const express = require("express");
 const app = express();
 
 function getTime() {
-	const date = new Date();
+  const date = new Date();
 
-	let dateAndTime = date.toLocaleString('en-US', {
-		timeZone: 'America/New_York',
-	});
+  let dateAndTime = date.toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+  });
 
-	return dateAndTime;
+  return dateAndTime;
 }
 
 app.use(function logger(req, res, next) {
-	console.log(`${getTime()} EST - ${req.method} ${req.path} - ${req.ip}`);
-	console.log("")
-	next();
+  console.log(`${getTime()} EST - ${req.method} ${req.path} - ${req.ip}`);
+  console.log("")
+  next();
 });
 
 app.get("/", (req, res) => {
-	res.sendFile(`${__dirname}/views/index.html`);
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.listen(3000, () => {
-	console.log("Server started");
+  console.log("Server started");
 });
+
+module.exports = app;
